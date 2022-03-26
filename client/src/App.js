7c164b16479685b033022f4d08a83ed47f4ae1c7
belalbahaa1 <belalbahaa800@gmail.com>
2022-03-26 08:11:11 +0200

    start

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="layout">
+      <Header />
+      <main>Content</main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../css/Header/Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Shopping Cart</h1>
+    </header>
+  );
+};
+
+export default Header;
