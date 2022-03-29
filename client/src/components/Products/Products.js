ac69085fda76e2a90430f066396e2feac5fcec66
belalbahaa1 <belalbahaa800@gmail.com>
2022-03-29 17:13:13 +0200

    product details

--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../css/Products/Products.css";
+import ProductModal from "./ProductModal";
 
 const Products = ({ products }) => {
+  const [product, setProduct] = useState("");
+
+  const openModal = (product) => {
+    setProduct(product);
+  };
   return (
     <div className="products-wrapper">
       {products.map((product) => (
         <div className="product-item" key={product.id}>
-          <img src={product.imageUrl} alt={product.title} />
+          <a href="#" onClick={() => openModal(product)}>
+            <img src={product.imageUrl} alt={product.title} />
+          </a>
           <div className="product-desc">
             <p>{product.title}</p>
-            <span>{product.price}</span>
+            <span>${product.price}</span>
           </div>
           <button className="product-btn">add to cart</button>
         </div>
       ))}
+
+      <ProductModal product={product} setProduct={setProduct} />
     </div>
   );
 };
